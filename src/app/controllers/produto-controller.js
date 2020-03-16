418dfaf3b87b6eff422a28338b9328a067952bf3
Paulo Frare <pfrare.tads@gmail.com>
2020-03-16 17:32:41 -0300

    tirando "?"

--- src/app/controllers/produto-controller.js
+++ src/app/controllers/produto-controller.js
@@ -3,8 +3,9 @@ var Produto = require("../models/produto");
 class ProdutoController {
   lista() {
     return (req, resp) => {
-      Produto.find( (err, produtos) => {
-        if (err) return resp.status(400).send(`Erro ao listar os produtos. ${err}`);
+      Produto.find((err, produtos) => {
+        if (err)
+          return resp.status(400).send(`Erro ao listar os produtos. ${err}`);
         resp.status(200).json(produtos);
       });
     };
@@ -14,9 +15,9 @@ class ProdutoController {
     return (req, resp) => {
       var produto = new Produto();
       //TODO: Assert fields
-      produto.nome = req.body?.nome;
-      produto.preco = req.body?.preco;
-      produto.descricao = req.body?.descricao;
+      produto.nome = req.body.nome;
+      produto.preco = req.body.preco;
+      produto.descricao = req.body.descricao;
 
       produto.save(err => {
         if (err) return resp.status(400).send(`Erro ao salvar produto: ${err}`);
@@ -39,12 +40,13 @@ class ProdutoController {
       Produto.findById(req.params.produto_id, (err, produto) => {
         if (err) return resp.status(400).send(`Produto não encontrado. ${err}`);
         //TODO: Criar Assert dos params
-        produto.nome = req.body?.nome;
-        produto.preco = req.body?.preco;
-        produto.descricao = req.body?.descricao;
+        produto.nome = req.body.nome;
+        produto.preco = req.body.preco;
+        produto.descricao = req.body.descricao;
 
         produto.save(err => {
-          if (err) return resp.status(400).send(`Erro ao atualizar o produto. ${err}`);
+          if (err)
+            return resp.status(400).send(`Erro ao atualizar o produto. ${err}`);
           resp.status(200).json({ message: "Produto Atualizado com sucesso" });
         });
       });
@@ -53,13 +55,10 @@ class ProdutoController {
 
   deleta() {
     return (req, resp) => {
-      Produto.remove(
-        { _id: req.params.produto_id },
-        err => {
-          if (err) return resp.status(400).send(`Produto não encontrado. ${err}`);
-          resp.status(200).json({ message: "Produto excluido com sucesso!" });
-        }
-      );
+      Produto.remove({ _id: req.params.produto_id }, err => {
+        if (err) return resp.status(400).send(`Produto não encontrado. ${err}`);
+        resp.status(200).json({ message: "Produto excluido com sucesso!" });
+      });
     };
   }
 }
